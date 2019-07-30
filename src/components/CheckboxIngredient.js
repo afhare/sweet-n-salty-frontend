8ad073aa3faa85aa00e73fa318267bbc4afd3bc1
afhare <alexandra.f.hare@gmail.com>
2019-07-30 18:46:58 -0500

    CSS styling, images, and logo

--- src/components/CheckboxIngredient.js
+++ src/components/CheckboxIngredient.js
@@ -14,7 +14,7 @@ class CheckboxIngredient extends React.Component {
     }
 
     componentDidMount(){
-        console.log(this.props)
+        //console.log(this.props)
     }
 
     handleCheckbox = () => {
@@ -53,13 +53,16 @@ class CheckboxIngredient extends React.Component {
     }
     render(){
         return (
-            <label>
-                {this.props.ingredient.name}
-                <input name={this.props.ingredient.name} type='checkbox' checked={this.state.checked} onChange={this.handleCheckbox}/>
+            <div className='ingredient-row'>
+                <div className='ingredient-name-and-checkbox'>
+                    <label>
+                        {this.props.ingredient.name}
+                    </label>
+                    <input name={this.props.ingredient.name} type='checkbox' checked={this.state.checked} onChange={this.handleCheckbox}/>
+                </div>
                 {this.state.checked && !this.state.confirmed ?
                 <div className='ingredient-amount-selection'>
-                <label>Select an amount:  </label>
-                    <br />
+                <p>Select an amount:  </p>
                     <select onChange={(e) => this.collectSnackIngredient(e)} name='amount'>
                         <option value='null'>Ingredient Amount:</option>
                         <option value='1/4'> 1/4 </option>
@@ -67,9 +70,8 @@ class CheckboxIngredient extends React.Component {
                         <option value='3/4'> 3/4 </option>
                         <option value='1'> 1 </option>
                     </select>
-                    <br />
-                <label>Select a size:  </label>
-                    <br />
+                    <br/>
+                <p>Select a size:  </p>
                     <select name='size' onChange={(e) => this.collectSnackIngredient(e)}>
                         <option value='null'>Amount Size:</option>
                         <option value='tablespoon'> tablespoon </option>
@@ -79,8 +81,8 @@ class CheckboxIngredient extends React.Component {
                     <button onClick={(e)=> this.confirmSnackIngredient(e)}>Confirm</button>
                 </div>
                     : null }
-                <br/>
-            </label>
+               
+            </div>
         )
     }
 }
